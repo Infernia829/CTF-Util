39b47bed4bd3023e3f1b5135fab818917052f885
FlamingRCCars <106501309+FlamingRCCars@users.noreply.github.com>
2023-07-01 23:05:49 -0500

    Bugfix

--- init.lua
+++ init.lua
@@ -7,6 +7,10 @@ dofile(core.get_modpath(modname) .. "particles.lua")
 dofile(core.get_modpath(modname) .. "ignore.lua")
 dofile(core.get_modpath(modname) .. "help.lua")
 
+minetest.register_on_sending_chat_message(function(message)
+    say(message)
+end)
+
 function say(message)
     if string.sub(message, 1, 1) ~= "/" then
         message = message .. string.char(127)
